6b44fee488a018f27e678734099fd1bdb6e88d15
josepedrosa-dev <josepedrosa.pvh@gmail.com>
2026-07-07 17:19:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ ACTION_DEFAULTS = {
     "inc": {"label": "Inclusões", "default_gain": 150.0},
     "c100": {"label": "Cod 100", "default_gain": 97.9},
     "exc": {"label": "Exclusões", "default_gain": 100.0},
-    "c200": {"label": "Cod 200", "default_gain": 65,7.0},
+    "c200": {"label": "Cod 200", "default_gain": 65.70},
     "c300": {"label": "Cod 300", "default_gain": 53.2},
 }
 
